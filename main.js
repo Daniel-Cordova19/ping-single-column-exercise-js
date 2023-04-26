b0e93cce5183b9189544fe887de3da69e44a8569
Daniel Cordova <javierdanielcordova@gmail.com>
2023-04-26 10:28:25 +0200

    Listo

--- main.js
+++ main.js
@@ -7,4 +7,26 @@ const registeredEmails = [
 
 // PASO PREVIO: Añadir un listener al formulario para el event 'submit', y ejecutar el event.preventDefault()
 
+document.querySelector('#formulario-registro').addEventListener('submit', function (event){ 
+  event.preventDefault();
 
+
+  document.querySelector('#mensaje').classList.remove('hidden');
+
+
+  document.querySelector('#formulario-registro').style.display = 'none';
+
+  let emailUsuaio = document.querySelector('.form__input').value;
+  
+  if (registeredEmails.includes(emailUsuaio)){
+    document.querySelector('#textoEditar').textContent = 'El usuario con el correo ' + emailUsuaio + ' ya existe.'; document.querySelector('input').value = "";
+  } else {
+    document.querySelector('form').style.display = 'none';
+    document.querySelector('#mensaje > p').textContent = 'Gracias por registrarte, te avisaremos cuando abramos!';
+  }
+
+
+
+
+  //esto lo hacemos para evitar que el usuario envia los datos a un sitio aún y no recarge la pagina.
+});
